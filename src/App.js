a3a5e8d9e6ba597aa153c73ffa4473b76645c67f
olasubomi <olasubomi.awokoya@hotmail.com>
2020-06-19 02:14:20 -0700

    pushing general react app to resolve react ui materials for integration to full stack app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import SignIn from "./SignIn";
+// import SignInSide from "./SignInSide";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello world
+      {/* <SignIn/> */}
+      {/* <SignInSide/> */}
+      {/* <Dashboard/> */}
+    </div>
+  );
+}
+
+export default App;
